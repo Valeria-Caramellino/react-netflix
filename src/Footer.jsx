8483296a4e452995f45ba7444a01ddc3a9d10a81
valeria caramellino <valeriacaramellino@gmail.com>
2023-11-12 10:41:21 +0100

    link footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,15 +1,27 @@
 import React from "react"
 
 const Footer = () =>{
+
+    const linkedin= 'https://www.linkedin.com/in/valeriacaramellino/';
+    const github='https://github.com/Valeria-Caramellino/react-netflix';
+
     return(
         <footer className='row align-items-center justify-content-center'>
             
             <div className="col-5 text-center d-flex align-items-center text-light justify-content-center">
-                <i className="fa-brands fa-linkedin mx-2"></i>
+                <a href={linkedin} target="_blank" className="text-light">
+
+                    <i className="fa-brands fa-linkedin mx-2"></i>
+               
+                </a>
                 
                 <p className="">Valeria Caramellino Progetto Netflix con React e Vite</p>
                 
-                <i className="fa-brands fa-github mx-2"></i>
+                <a href={github} target="_blank" className="text-light">
+                    
+                    <i className="fa-brands fa-github mx-2"></i>
+
+                </a>
             </div>
            
         </footer>
